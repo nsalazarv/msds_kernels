0999ce8d4edf96a9ca10fff26f759e604f0788bd
Nelson Salazar Valdebenito <nsalazarv@Nelsons-MacBook-Pro.local>
2022-09-21 23:03:39 -0300

    Avance

--- Tarea 2/T2Kernels.R
+++ Tarea 2/T2Kernels.R
@@ -14,13 +14,13 @@ data['grupo'] <- index
 train <- data[which(data$grupo == 1 | data$grupo == 2 | data$grupo == 3),]
 test <- data[which(data$grupo == 4),]
 
-x1_train = train[1]
-x2_train = train[2]
-y_train = train[3]
+x1_train = unlist(train[1])
+x2_train = unlist(train[2])
+y_train = unlist(train[3])
 
-x1_test = test[1]
-x2_test = test[2]
-y_test = test[3]
+x1_test = unlist(test[1])
+x2_test = unlist(test[2])
+y_test = unlist(test[3])
 
 # Estandarizando 
 
@@ -40,4 +40,25 @@ Kfun = function(xi,xj,l2=0.5)
 {
   exp(-sum((xi-xj)^2)/l2)
 }
+
+# Matriz de Gram
+
+ntrain = 151
+K = matrix(0,nrow=ntrain,ncol=ntrain)
+
+for(i in 1:ntrain)
+{
+  for(j in 1:ntrain)
+  {
+    K[i,j] = Kfun(x1train_est[i], x2train_est[j])
+  }
+}
+
+# Calculando W hat e Y hat
+
+lambda = 1
+In=diag(ntrain)
+w_hat = solve(K + lambda*In)%*%y_train
+
+y_hat=K%*%w_hat
 
