c6ca96d95c2a50348bd7ba6d7757946c506165c2
darckmate <65508669+darckmate@users.noreply.github.com>
2022-09-29 15:43:11 -0400

    Update T2Kernels.R
    agrego pruebas de la 2.4

--- Tarea 2/T2Kernels.R
+++ Tarea 2/T2Kernels.R
@@ -254,3 +254,135 @@ boxplot(unlist(l_vec), main='L')
 boxplot(unlist(lam_vec), main='Lambda')
 #Tenemos que los valores de LS tienen mayor varianza en sus distribucion en contraste con los valores lambda que se mantienen en un rango mucho menor, teniendo diferencias considerables al caso previo
 
+#2.4
+l1_vec = c()
+l2_vec = c()
+lam_vec = c()
+ecm_vec4 = c()
+rsq_vec4 = c()
+
+l1_grid = seq(0.1, 1, 0.1)
+l2_grid = seq(0.1, 1, 0.1)
+lam_grid = seq(0.1, 1, 0.1)
+
+for(i in 1:10){
+  
+  print(paste("Fold: ", i))
+  
+  llgrid = expand.grid(l1_grid,l2_grid, lam_grid)
+  dim_grid = dim(llgrid)[1]
+  
+  ecm_save = rep(0, dim_grid)
+  
+  testIndexes <- which(folds==i,arr.ind=TRUE)
+  test <- data[testIndexes, ]
+  train <- data[-testIndexes, ]
+  
+  x_train = train[1:2]
+  y_train = unlist(train[3])
+  
+  x_test = test[1:2]
+  y_test = unlist(test[3])
+  
+  # Estandarizando 
+  
+  mux = apply(x_train,2,mean)
+  sdx = apply(x_train,2,sd)
+  
+  xtrain_est = x_train
+  xtrain_est[,1] = (x_train[,1] - mux[1])/sdx[1]
+  xtrain_est[,2] = (x_train[,2] - mux[2])/sdx[2]
+  
+  xtest_est = x_test
+  xtest_est[,1] = (x_test[,1] - mux[1])/sdx[1]
+  xtest_est[,2] = (x_test[,2] - mux[2])/sdx[2]
+  
+  
+  #############################PRobado hasta aka
+  # Función de Kernel
+  
+  Kfun = function(xi,xj,l1,l2)
+  {
+    exp(((-((xi-xj)/l1)^2)-((xi-xj)/l2)^2))
+  }
+  
+  # Obteniendo l y lambda
+  
+  for(z in 1:dim_grid){
+    for(y in 1:dim_grid){
+    
+    print(paste("Iter: ", z))
+    
+    ntrain = dim(xtrain_est)[1]
+    K = array(0, c(ntrain, ntrain,ntrain))
+    
+    for(j in 1:ntrain){
+      for(k in 1:ntrain){
+        K[j,k,z] = Kfun(xtrain_est[j,], xtrain_est[k,], llgrid[z,1],llgrid[z,2])
+        }
+      }
+    }
+    
+    w_hat = solve(t(K)%*%K+llgrid[z,3]*diag(ntrain))%*%t(K)%*%y_train
+    
+    ntest = dim(xtest_est)[1]
+    K_pred_test=matrix(0, nrow = ntest, ncol = ntrain)
+    
+    for(j in 1:ntrain){
+      K_pred_test[,j]=exp(-rowSums((xtest_est - matrix(unlist(xtrain_est[j,]), nrow = ntest, ncol = 2, byrow = TRUE))^2)/(llgrid[z,1]^2))
+    }
+    
+    y_pred_test = K_pred_test%*%w_hat
+    
+    ecm_save[z] = 1/ntest*sum((y_test - y_pred_test)^2)
+  }
+  
+  #Encontramos los parámetros óptimos:
+  
+  index = which(ecm_save == min(ecm_save))
+  llgrid[index,]
+  
+  l1_vec = append(l1_vec, llgrid[index,][1])
+  l2_vec = append(l2_vec, llgrid[index,][2])
+  lam_vec = append(lam_vec, llgrid[index,][3])
+  
+  ## Calculando en base a los valores obtenidos
+  
+  print(l1_vec[i])
+  print(l2_vec[i])
+  print(lam_vec[i])
+  
+  ntrain = dim(xtrain_est)[1]
+  K = matrix(0, nrow = ntrain, ncol = ntrain)
+  
+  for(j in 1:ntrain){
+    for(k in 1:ntrain){
+      K[j,k] = Kfun(xtrain_est[j,], xtrain_est[k,], unlist(l1_vec[i]), unlist(l2_vec[i]))
+    }
+  }
+  
+  w_hat = solve(t(K)%*%K+unlist(lam_vec[i])*diag(ntrain))%*%t(K)%*%y_train
+  
+  ntest = dim(xtest_est)[1]
+  K_pred_test=matrix(0, nrow = ntest, ncol = ntrain)
+  
+  for(j in 1:ntrain){
+    K_pred_test[,j]=exp(-rowSums((xtest_est - matrix(unlist(xtrain_est[j,]), nrow = ntest, ncol = 2, byrow = TRUE))^2)/(unlist(l_vec[i])^2))
+  }
+  
+  y_pred_test = K_pred_test%*%w_hat
+  
+  ecm = 1/ntest*sum((y_test - y_pred_test)^2)
+  ecm_vec4 = append(ecm_vec4,ecm)
+  
+  rsq = cor(y_test, y_pred_test)^2
+  rsq_vec4 = append(rsq_vec4, rsq)
+  print(rsq_vec4)
+  print(ecm_vec4)
+}  
+
+l1_vec
+l2_vec
+lam_vec
+ecm_vec4
+rsq_vec4
